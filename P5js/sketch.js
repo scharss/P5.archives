f4c00b77f13a753ca3926eb997e9b86f252e3f32
Scharss <scharss@gmail.com>
2019-01-05 02:28:51 -0500

    Archives to work with P5

---
+++ P5js/sketch.js
@@ -0,0 +1,19 @@
+var x=3;
+var y= 4;
+ function setup() {
+	 createCanvas(600, 400);
+	 background(102);
+ 
+ }
+ 
+  
+ function draw() {
+	 if(mouseIsPressed){
+		 document.write("Hola Mundo!");
+	 } else{
+		 
+		 ellipse(300,100,300,150)
+	 }
+	
+ }
+ 
